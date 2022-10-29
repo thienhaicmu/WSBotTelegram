c600b8a570bf4702d6976b90198c70a3d8135328
Ng Thanh Cong <nxgthanhcong@gmail.com>
2022-10-29 20:12:55 +0700

    first commit

---
+++ WSBotTele/Services/RedisClient.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WSBotTele.Services
+{
+    interface IRedisClient
+    {
+        bool SetValue(string key, dynamic values);
+        object GetValue(string key);
+    }
+    class RedisClient : IRedisClient
+    {
+
+        private static ConnectionMultiplexer redisServer = ConnectionMultiplexer.Connect("localhost:6379");
+        private static IDatabase multiDB = redisServer.GetDatabase();
+
+        public bool SetValue(string key, dynamic values)
+        {
+            return multiDB.StringSet(key: key, value: JsonConvert.SerializeObject(values), expiry: TimeSpan.FromSeconds(200));
+        }
+        public object GetValue(string key)
+        {
+            return multiDB.StringGet(key: key);
+        }
+    }
+}
